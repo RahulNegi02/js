fe3a9b1010abd863d7ba8c0dc81855631af672d5
RahulNegi02 <rahulnegi02f@gmail.com>
2026-09-06 23:43:37 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+//alert("Hello World");//we are suing nodejs ,not browser so alert will not work
+console.log("Hello World");//this will work in nodejs
+console.log("3+4")
+
+
+
+let name = "Rahul"
+let age = 22
+let isLoggedIn = false
+
+
+//number => 2 to the power of 53
+//bigint => greater than 2 to the power of 53
+//string => any text inside single or double quotes
+//boolean => true or false
+//null => empty value
+//undefined => value is not assigned
+//symbol => unique value
+//object => key value pair
+
+console.log(typeof null);
+console.log(typeof undefined);
