e8867592e21b6f995239625cfa951719d339cca4
RahulNegi02 <rahulnegi02f@gmail.com>
2026-09-06 23:24:04 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,10 @@
+const accountID = 1234
+let accountEmail = "rahul@gmail.com"
+var accountPassword = "rahul@123"
+let accountCity = "Delhi"
+
+//accountID = 5678
+accountEmail = "rahul123@gmail.com"
+console.log(accountID);
+console.log(accountEmail);
+console.table([accountPassword,accountCity,accountID,accountEmail]);
