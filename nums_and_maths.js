008bfa3ae0032bae02f397a66bb285ee5e732dea
RahulNegi02 <rahulnegi02f@gmail.com>
2026-09-08 01:27:24 +0530

    nums_and_maths.js

---
+++ nums_and_maths.js
@@ -0,0 +1,20 @@
+const score = 400
+console.log(score)
+const balance = new Number(400)
+console.log(balance)
+
+
+console.log(balance.toString())
+ console.log(balance.toFixed(2))
+
+ const othernumber = 112.49
+ console.log(othernumber.toPrecision(3))
+
+ const number = 123456789
+ console.log(number.toLocaleString('en-IN'));
+console.log(number.toLocaleString('en-JP'));
+
+
+// +++++++++++++++++ maths ++++++++++++++++++++++++++++
+
+// console.log(Math.pi)
