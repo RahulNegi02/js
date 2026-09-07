0acd5d879e9cc454423e1c86a40447373cc7cbfd
RahulNegi02 <rahulnegi02f@gmail.com>
2026-09-08 01:04:46 +0530

    string.js

---
+++ string.js
@@ -0,0 +1,27 @@
+const name = "Rahul";
+const age = 25;
+
+// console.log(name +age + "value")
+// console.log(`my name is ${name} and my age is ${age}`)
+
+
+const gameName = new String("Cricket");
+// console.log(gameName);
+// console.log(gameName.__proto__)
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.indexOf("r"))
+// console.log(gameName.charAt(6))
+
+// const newgame = (gameName.substring(0, 5))
+// console.log(newgame)
+
+// const Name =("    rahul negi  ")
+// console.log(Name.trim())
+
+const url = "https://ww.Rahul.com/Rahul%20Negi"
+console.log(url.replace("%20", "-"))
+console.log(url.includes('%20'))
+
+console.log(url.split("-"))
