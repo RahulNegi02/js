27364f8de3bdb91bf10b8811312248fc7afcb59c
RahulNegi02 <rahulnegi02f@gmail.com>
2026-09-08 01:40:19 +0530

    maths

--- nums_and_maths.js
+++ nums_and_maths.js
@@ -1,20 +1,32 @@
-const score = 400
-console.log(score)
-const balance = new Number(400)
-console.log(balance)
+// const score = 400
+// console.log(score)
+// const balance = new Number(400)
+// console.log(balance)
 
 
-console.log(balance.toString())
- console.log(balance.toFixed(2))
+// console.log(balance.toString())
+//  console.log(balance.toFixed(2))
 
- const othernumber = 112.49
- console.log(othernumber.toPrecision(3))
+//  const othernumber = 112.49
+//  console.log(othernumber.toPrecision(3))
 
- const number = 123456789
- console.log(number.toLocaleString('en-IN'));
-console.log(number.toLocaleString('en-JP'));
+//  const number = 123456789
+//  console.log(number.toLocaleString('en-IN'));
+// console.log(number.toLocaleString('en-JP'));
 
 
 // +++++++++++++++++ maths ++++++++++++++++++++++++++++
 
-// console.log(Math.pi)
+console.log(Math)
+console.log(Math.abs(-9))
+console.log(Math.round(4.6))
+console.log(Math.floor(4.9))
+console.log(Math.ceil(4.1))
+console.log(Math.pow(2, 3))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random() * 10000 +1))
+
+const min = 1000
+const max = 10000
+console.log (Math.floor(Math.random() * (max - min + 1) + min))
